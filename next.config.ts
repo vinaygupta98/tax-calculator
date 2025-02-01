041f64b4331bf18697d7e8084c42e8d9fd908c8d
Uttam Gupta <uttam.gupta@nectr.com.au>
2025-02-01 20:04:22 +0530

    add github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",  
+  basePath: "/tax-calculator",
 };
 
 export default nextConfig;
